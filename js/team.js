484aafec7dcffe96a7d04baffe3810134d31aed8
lorenzotroiano <lorenzo.troiano1999@gmail.com>
2023-05-12 15:04:20 +0200

    inseriti i dati, creando arrey di oggetti

---
+++ js/team.js
@@ -0,0 +1,68 @@
+/*
+
+Utilizzando i dati forniti, creare un array di oggetti per rappresentare i membri del team.
+Ogni membro è caratterizzato dalle seguenti informazioni: nome, ruolo e foto.
+MILESTONE 0:
+Creare l’array di oggetti con le informazioni fornite.
+MILESTONE 1:
+Stampare su console, per ogni membro del team, le informazioni di nome, ruolo e la stringa della foto
+MILESTONE 2:
+Stampare le stesse informazioni su DOM sottoforma di stringhe
+
+
+
+
+
+
+Dati
+Wayne Barnett	Founder & CEO	wayne-barnett-founder-ceo.jpg
+Angela Caroll	Chief Editor	angela-caroll-chief-editor.jpg
+Walter Gordon	Office Manager	walter-gordon-office-manager.jpg
+Angela Lopez	Social Media Manager	angela-lopez-social-media-manager.jpg
+Scott Estrada	Developer	scott-estrada-developer.jpg
+Barbara Ramos	Graphic Designer	barbara-ramos-graphic-designer.jpg
+*/
+
+
+
+const elTeam = [
+
+    { 
+        nome: "Wayne Barnett",
+        posizione: "Chief Editor",
+        foto: "wayne-barnett-founder-ceo.jpg",
+    },
+
+    { 
+        nome: "Angela Caroll",
+        posizione: "Founder & CEO",
+        foto: "angela-caroll-chief-editor.jpg",
+    },
+
+    { 
+        nome: "Walter Gordon",
+        posizione: "Office Manager",
+        foto: "walter-gordon-office-manager.jpg",
+    },
+
+    { 
+        nome: "Angela Lopez",
+        posizione: "Social Media Manager",
+        foto: "angela-lopez-social-media-manager.jpg",
+    },
+
+    { 
+        nome: "Scott Estrada",
+        posizione: "Developer",
+        foto: "scott-estrada-developer.jpg",
+    },
+
+    { 
+        nome: "Barbara Ramos",
+        posizione: "Graphic Designer",
+        foto: "barbara-ramos-graphic-designer.jpg"
+    }
+]
+
+
+console.log(elTeam);
